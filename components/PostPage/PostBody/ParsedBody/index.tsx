d0aa53df0d1fe350df446159917f012c1300915e
Asunaro276 <ryuhei276276@gmail.com>
2022-09-28 03:25:04 +0900

    cheerioでのパースに修正

--- components/PostPage/PostBody/ParsedBody/index.tsx
+++ components/PostPage/PostBody/ParsedBody/index.tsx
@@ -1,49 +1,55 @@
-import { FileCopyRounded, FileCopySharp } from "@mui/icons-material"
 import { Box, BoxProps, IconButton, Typography } from "@mui/material"
-import { useState } from "react"
 import { BlogBody, Code, Link, Paragraph } from "types"
 import { LinkCard } from "./LinkCard"
 import { MarkdownTemplate } from "./MarkdownTemplate"
 
 type Props = {
-  body: BlogBody
+  body: string
 } & BoxProps
+
+// export const ParsedBody = (props: Props) => {
+//   return (
+//     <Box>
+//       {
+//         props.body.map((field, index) => {
+//           switch (field.fieldId) {
+//             case "paragraph":
+//               return <MarkdownTemplate html={(field as Paragraph).paragraph} key={index} />
+//             case "code":
+//               return (
+//                 <Box className="relative">
+//                   <Typography className="absolute text-center inline-block px-2 text-white bg-slate-500">
+//                     {(field as Code).fileName}
+//                   </Typography>
+//                   <MarkdownTemplate html={(field as Code).code} key={index} /> 
+//                 </Box>
+//               )
+//             case "link":
+//               const link = {
+//                 fieldId: field.fieldId,
+//                 url: (field as Link).url,
+//                 title: (field as Link).title,
+//                 image: (field as Link).image,
+//                 linkTo: (field as Link).linkTo,
+//               }
+//               return (
+//                 <Box className="mb-2">
+//                   <LinkCard link={link} key={index} />
+//                 </Box>
+//               )
+//             default:
+//               return <></>
+//           }
+//         })
+//       }
+//     </Box>
+//   )
+// }
 
 export const ParsedBody = (props: Props) => {
   return (
     <Box>
-      {
-        props.body.map((field, index) => {
-          switch (field.fieldId) {
-            case "paragraph":
-              return <MarkdownTemplate html={(field as Paragraph).paragraph} key={index} />
-            case "code":
-              return (
-                <Box className="relative">
-                  <Typography className="absolute text-center inline-block px-2 text-white bg-slate-500">
-                    {(field as Code).fileName}
-                  </Typography>
-                  <MarkdownTemplate html={(field as Code).code} key={index} /> 
-                </Box>
-              )
-            case "link":
-              const link = {
-                fieldId: field.fieldId,
-                url: (field as Link).url,
-                title: (field as Link).title,
-                image: (field as Link).image,
-                linkTo: (field as Link).linkTo,
-              }
-              return (
-                <Box className="mb-2">
-                  <LinkCard link={link} key={index} />
-                </Box>
-              )
-            default:
-              return <></>
-          }
-        })
-      }
+      {props.body}
     </Box>
   )
 }
--- components/PostPage/PostBody/index.tsx
+++ components/PostPage/PostBody/index.tsx
@@ -1,12 +1,12 @@
 import { Box, Card } from "@mui/material"
 import { parsePostBody } from "libs/parsePostBody";
-import { Blog, Heading } from "types"
+import { Blog, Heading, ParsedBlog } from "types"
 import Caption from "./Caption"
 import { ParsedBody } from "./ParsedBody";
 import { TableOfContents } from "./TableOfContents";
 
 type Props = {
-  blog: Blog
+  blog: ParsedBlog
   headings: Heading[]
 }
 
@@ -14,19 +14,20 @@ const PostBody = (props: Props) => {
   return (
   <Card className="my-10">
     <Box className="my-10">
-      <Caption
+      {/* <Caption
         title={props.blog.title}
         publishedAt={props.blog.publishedAt}
         imageUrl={props.blog.image.url}
         imageAlt={props.blog.imageAlt}
-      />
+      /> */}
     </Box>
     <Box className="flex justify-center mb-20">
-      <TableOfContents heading={parsePostBody(props.blog.body)} />
+      <TableOfContents heading={props.headings} />
     </Box>
     <Box className="flex justify-center">
       <Box className="w-10/12 mb-20">
-        <ParsedBody body={props.blog.body} />
+        {/* <ParsedBody body={props.blog.body} /> */}
+        <div dangerouslySetInnerHTML={{ __html: props.blog.body }} />
       </Box>
     </Box>
   </Card>
