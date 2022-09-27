7e89e6348e8cc3d29c77b075ee4f76170b3f003c
Asunaro276 <ryuhei276276@gmail.com>
2022-09-27 20:42:20 +0900

    add next-export-optimize-images

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
-const rehypePrism = require("@mapbox/rehype-prism");
-const withMDX = require('@next/mdx')({
-extension: /\.mdx?$/,
-options: {
-  remarkPlugins: [],
-  rehypePlugins: [rehypePrism]
-  // If you use `MDXProvider`, uncomment the following line.
-  // providerImportSource: "@mdx-js/react",
-},
-})
-module.exports = withMDX({
+const withExportImages = require('next-export-optimize-images')
+module.exports = withExportImages({
 // Append the default value with md extensions
-pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
-
-module.exports = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   images: {
     domains: ["images.microcms-assets.io"],
   },
-}
+})
