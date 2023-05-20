34dd0dfe64e6b17345e9d00d357c345a4cb0cf64
asunaro276 <ryuhei276276@gmail.com>
2023-05-20 23:18:52 +0900

    [fix] likCardの画像のレイアウト崩れを修正

--- libs/parse/parseBody.ts
+++ libs/parse/parseBody.ts
@@ -80,8 +80,8 @@ export const parseBody = async (body: string) => {
         $(element).replaceWith(`
           <div class="shadow-md shadow-outline bg-slate-100 mt-4 mb-20 hover:brightness-[0.9] duration-300 ease-out">
             <a class="no-underline" href=${linkUrl} target="_blank" rel="noopener noreferrer">
-              <div class="flex flex-col lg:flex-row items-center p-2">
-                <img src=${og["image"] === undefined ? amazonImage : og["image"]} class="self-center w-[90%] max-w-[20rem] max-h-[20rem] mt-2 lg:mt-2 lg:mr-3" />
+              <div class="flex flex-col lg:flex-row items-center justify-center p-2">
+                <img src=${og["image"] === undefined ? amazonImage : og["image"]} class="self-center w-[90%] max-w-[12rem] max-h-[20rem] mt-2 lg:mt-2 lg:mr-3" />
                 <div class="flex flex-col w-[90%] lg:w-1/2 justify-center items-center">
                   <p class="font-bold my-3 w-full break-words">
                     ${og["title"]}
