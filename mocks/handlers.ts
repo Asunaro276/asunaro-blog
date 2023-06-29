62466f68a5ddc6a02d89029556602cb6a3f77641
asunaro276 <ryuhei276276@gmail.com>
2023-06-29 23:49:29 +0900

    [add] testデータを追加

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,10 +1,16 @@
 import { rest } from 'msw'
-import data from './test-data.json'
+import data from './test-data/index.json'
 import { NewtResponse } from 'types'
 
 const baseUrl = `https://${process.env.spaceUid}.api.newt.so/v1/`
 
 export const handlers = [
+  rest.get(`${baseUrl}/asunaroblog/article`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json<NewtResponse>(data)
+    )
+  }),
   rest.get(`${baseUrl}/asunaroblog/article`, (req, res, ctx) => {
     return res(
       ctx.status(200),
--- mocks/index.ts
+++ mocks/index.ts
@@ -2,9 +2,11 @@ async function initMocks() {
   if (typeof window === 'undefined') {
     const { server } = await import('./server')
     server.listen()
+    console.log('Mocking enabled on server')
   } else {
     const { worker } = await import('./browser')
     worker.start()
+    console.log('Mocking enabled on browser')
   }
 }
 
