375b264d37847f4d35a00a0eb2c1cec40dfc9a31
asunaro276 <ryuhei276276@gmail.com>
2023-11-23 21:38:54 +0900

    [add] Xとgithubのアイコンを追加

--- components/common/SideBar/SideProfile/index.tsx
+++ components/common/SideBar/SideProfile/index.tsx
@@ -1,5 +1,7 @@
-import { Card, CardContent, CardMedia, Divider, Typography } from '@mui/material'
+import { GitHub } from '@mui/icons-material'
+import { Button, Card, CardContent, CardMedia, Divider, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
+import { XIcon } from './XIcon'
 
 export const SideProfile = () => {
   return (
@@ -18,6 +20,15 @@ export const SideProfile = () => {
           <Typography className='text-center'>あすなろ</Typography>
           <Box className='flex justify-center'>
             <Divider className='w-20' />
+          </Box>
+          <Box sx={{ marginX: '40px', marginY: '20px' }}>
+            <Button href='https://github.com/Asunaro276/asunaro-blog' target='_blank' sx={{ padding: '0px', minWidth: 'unset' }}>
+              <GitHub sx={{ color: '#000', fontSize: '22px' }}/>
+            </Button>
+            <Button href='https://twitter.com/asunaro276' target='_blank' sx={{ padding: '0px', minWidth: 'unset', marginLeft: '10px' }}>
+              <XIcon  sx={{ color: '#000', fontSize: '22px' }} />
+            </Button>
+
           </Box>
           <Typography className='my-10 px-10'>
             新米エンジニアの技術ブログです。主にWeb技術や数学で遊んでいます。日々楽しみながら学んでいくことを目標としています。
