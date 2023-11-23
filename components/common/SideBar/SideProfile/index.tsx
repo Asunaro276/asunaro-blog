1c9f81ae3c0d32072d9924a4d3d2e53c313b23b9
asunaro276 <ryuhei276276@gmail.com>
2023-11-23 22:10:07 +0900

    [update] tailwindを使わないようにリファクタ

--- components/common/SideBar/SideProfile/index.tsx
+++ components/common/SideBar/SideProfile/index.tsx
@@ -5,36 +5,34 @@ import { XIcon } from './XIcon'
 
 export const SideProfile = () => {
   return (
-    <Box className='h-full justify-end'>
-      <Card className='h-full'>
-        <CardContent className=''>
-          <Typography className='my-5 text-center'>profile</Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'center', marginY: '2rem' }}>
-            <CardMedia
-              component='img'
-              src='https://storage.googleapis.com/p_641d41d3a492e5ac4c9226fe/3ab0e1e0-e610-4dc9-acb4-f7917add37b1/asunaro.png'
-              height='150'
-              sx={{ width: '150px', borderRadius: '100%' }}
-            />
-          </Box>
-          <Typography className='text-center'>あすなろ</Typography>
-          <Box className='flex justify-center'>
-            <Divider className='w-20' />
-          </Box>
-          <Box sx={{ marginX: '40px', marginY: '20px' }}>
-            <Button href='https://github.com/Asunaro276/asunaro-blog' target='_blank' sx={{ padding: '0px', minWidth: 'unset' }}>
-              <GitHub sx={{ color: '#000', fontSize: '22px' }}/>
-            </Button>
-            <Button href='https://twitter.com/asunaro276' target='_blank' sx={{ padding: '0px', minWidth: 'unset', marginLeft: '10px' }}>
-              <XIcon  sx={{ color: '#000', fontSize: '22px' }} />
-            </Button>
-
-          </Box>
-          <Typography className='my-10 px-10'>
-            新米エンジニアの技術ブログです。主にWeb技術や数学で遊んでいます。日々楽しみながら学んでいくことを目標としています。
-          </Typography>
-        </CardContent>
-      </Card>
-    </Box>
+    <Card>
+      <CardContent >
+        <Typography sx={{ textAlign: 'center', marginY: '18px' }}>profile</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', marginY: '2rem' }}>
+          <CardMedia
+            component='img'
+            src='https://storage.googleapis.com/p_641d41d3a492e5ac4c9226fe/3ab0e1e0-e610-4dc9-acb4-f7917add37b1/asunaro.png'
+            height='150'
+            sx={{ width: '150px', borderRadius: '100%' }}
+          />
+        </Box>
+        <Typography sx={{ textAlign: 'center' }}>あすなろ</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Divider sx={{ width: '80px' }}/>
+        </Box>
+        <Box sx={{ marginX: '40px', marginY: '20px' }}>
+          <Button href='https://github.com/Asunaro276/asunaro-blog' target='_blank' sx={{ padding: '0px', minWidth: 'unset' }}>
+            <GitHub sx={{ color: '#000', fontSize: '22px' }}/>
+          </Button>
+          <Button href='https://twitter.com/asunaro276' target='_blank' sx={{ padding: '0px', minWidth: 'unset', marginLeft: '10px' }}>
+            <XIcon  sx={{ color: '#000', fontSize: '22px' }} />
+          </Button>
+        
+        </Box>
+        <Typography sx={{ margin: '40px'}}>
+          新米エンジニアの技術ブログです。主にWeb技術や数学で遊んでいます。日々楽しみながら学んでいくことを目標としています。
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
